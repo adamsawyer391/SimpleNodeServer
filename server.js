e1037b2f0801de6956524d4129e525a8f6d0a551
AboveCosmic333 <39273302+adamsawyer391@users.noreply.github.com>
2021-08-25 07:16:33 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+
+    if(req.url == '/'){
+        res.setHeader("Content-type", "application/json");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.writeHead(200);
+        let dataObject = { id: 123, name: 'Adam', email: 'someone@email.com' };
+        let data = JSON.stringify(dataObject);
+        res.end(data);
+    }
+    else if(req.url == '/student'){
+        res.setHeader('Content-type', 'text/html');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(200);
+        let page = '<h1>Hello World, Student!</h1>';
+        res.end(page);
+    }
+    
+})
+
+server.listen(80, function() {
+    console.log("Server is running at 3000");
+})
